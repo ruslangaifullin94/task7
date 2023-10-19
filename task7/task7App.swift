014eadfd21d58319cca4d7b49ddba9a76bb9cacd
ri.gayfullin <ri.gayfullin@tensor.ru>
2023-10-19 16:40:18 +0300

    Initial Commit

---
+++ task7/task7App.swift
@@ -0,0 +1,17 @@
+//
+//  task7App.swift
+//  task7
+//
+//  Created by Руслан Гайфуллин on 19.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct task7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
